e89d71fecb0569e3662f272d40791213005fe478
Roshini349 <vvce24cse0241@vvce.ac.in>
2026-09-22 13:02:41 +0530

    hgfjhgjh

--- test.py
+++ test.py
@@ -4,3 +4,12 @@ def addition(x,y)
     print("sum=",sum)
 add(10,20)
 print("VVCE")
+print("HI");
+def asum(x,y)
+def addition(x,y)
+    sum=x+y
+    print("sum=",sum)
+add(10,20)
+print("VVCE")
+print("HI");
+32132132132
